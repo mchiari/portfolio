c9bd8ed2d7116f500123d0fccc8aa274f09d637d
Matheus Chiari <mchiaridev@gmail.com>
2022-07-19 00:24:47 -0300

    quick fix

--- src/structure/Header.tsx
+++ src/structure/Header.tsx
@@ -1,53 +1,74 @@
-import { StarIcon } from '@chakra-ui/icons'
-import { Box, Button, Heading, Square, Text } from '@chakra-ui/react'
-import { useState } from 'react';
+import { StarIcon } from "@chakra-ui/icons";
+import { Box, Button, Heading, Square, Text } from "@chakra-ui/react";
+import { useState } from "react";
 
 const Header = () => {
-	const [color, setColor] = useState("");
+	const [color, setColor] = useState(false);
 
 	const handleScroll = () => {
 		if (window.scrollY > 0) {
 			if (!color) {
-				setColor("red");
+				setColor(true);
 			}
 		} else {
 			if (color) {
-				setColor("");
+				setColor(false);
 			}
 		}
 	};
 
 	window.addEventListener("scroll", handleScroll);
 
-    return (
-        <Box
-        bgColor={!color ? "transparent" : "black"}
-            position={'fixed'}
-            zIndex={'sticky'} w={'100%'}
-            h={'100px'}
-            color={!color ? "black" : "white"}
-            display={'flex'}
-            flexDir={'row'}
-            justifyContent={'space-around'}
-            alignItems={'center'}
-            transition={"ease-in-out 500ms"}
-        >
-            <Box display={'flex'}>
-                <Square px={5}>
-                    <StarIcon />
-                </Square>
-                <Heading as={'h3'} color={!color ? "black" : "white"} size={'lg'}>@mchiaridev</Heading>
-            </Box>
-            <Box display={'flex'} flexDirection={'row'} justifyContent={'center'} alignItems={'center'} gap={5} fontWeight={'bold'}>
+	return (
+		<Box
+			bgColor={!color ? "transparent" : "black"}
+			position={"fixed"}
+			zIndex={"sticky"}
+			w={"100%"}
+			h={"100px"}
+			color={!color ? "black" : "white"}
+			display={"flex"}
+			flexDir={"row"}
+			justifyContent={"space-around"}
+			alignItems={"center"}
+			transition={"ease-in-out 500ms"}
+		>
+			<Box display={"flex"}>
+				<Square px={5}>
+					<StarIcon />
+				</Square>
+				<Heading
+					as={"h3"}
+					color={!color ? "black" : "white"}
+					size={"lg"}
+				>
+					@mchiaridev
+				</Heading>
+			</Box>
+			<Box
+				display={"flex"}
+				flexDirection={"row"}
+				justifyContent={"center"}
+				alignItems={"center"}
+				gap={5}
+				fontWeight={"bold"}
+			>
+				<a href={"#home"}>
+					<Text>Home</Text>
+				</a>
+				<a href={"#portfolio"}>
+					<Text>Recent work</Text>
+				</a>
+				<a href={"#lifeExperience"}>
+					<Text>Life Experience</Text>
+				</a>
 
-                <a href={'#home'}><Text>Home</Text></a>
-                <a href={'#portfolio'}><Text>Recent work</Text></a>
-                <a href={'#lifeExperience'}><Text>Life Experience</Text></a>
+				<Button colorScheme={!color ? "navy" : "orange"} ml={"20px"}>
+					Send me a message
+				</Button>
+			</Box>
+		</Box>
+	);
+};
 
-                <Button colorScheme={!color ? "navy" : "orange"} ml={'20px'}>Send me a message</Button>
-            </Box>
-        </Box>
-    )
-}
-
-export default Header
+export default Header;
